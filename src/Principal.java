25963649250b6e33ae5f1046c1dae30f0d1f20c2
limacebem@gmail.com <limacebem@gmail.com>
2021-10-05 11:39:06 +0200

    creada la web inicial

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args){
+        int c74wounter = 1;
+        while(counter <= 1000){
+            System.out.println(counter);
+            counter = counter + 1;
+        }
+    }
+}
